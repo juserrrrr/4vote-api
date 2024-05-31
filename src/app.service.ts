79d84e5a44690209b1248734fca59041b83675a7
brendatrindade <brendatrindde@gmail.com>
2024-05-31 13:14:50 -0300

    BT - first pull request

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!<br>Fernanda<br>Gabriel Baptista';
+    return 'Hello World!<br>Fernanda<br>Gabriel Baptista<br>Brenda Trindade';
   }
 }
