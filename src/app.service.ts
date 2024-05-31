3bf6fd571c0f1c15d1fa7c52803b077236f22510
Amanda <limaa9000@gmail.com>
2024-05-31 14:27:37 -0300

    AL - First pull request

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,7 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!<br>Fernanda';
-    return 'Hello World!<br>Amanda';
+    return 'Hello World!<br>Fernanda<br>Gabriel Baptista<br>Brenda Trindade<br>Pedro Mendes<br>Amanda';
   }
 }
