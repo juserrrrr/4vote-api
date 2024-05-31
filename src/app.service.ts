155acc24382271d338e13e03a7fa90b8778d876a
Gabriel Baptista <gabriel.costa.baptista1@gmail.com>
2024-05-31 12:17:11 -0300

    gb-firstpull

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!<br>Fernanda';
+    return 'Hello World!<br>Fernanda<br>Gabriel Baptista';
   }
 }
