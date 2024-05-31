85658c941ca34f1325d7451f46c374b079e20123
Pedro Mendes <99758612+mendes658@users.noreply.github.com>
2024-05-31 14:05:11 -0300

    PM - first PR

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!<br>Fernanda<br>Gabriel Baptista<br>Brenda Trindade';
+    return 'Hello World!<br>Fernanda<br>Gabriel Baptista<br>Brenda Trindade<br>Pedro Mendes';
   }
 }
