ed23d7c18f71add265343103a15c7cda152df63e
Thiago <thiagopinto.sena@gmail.com>
2024-05-31 14:33:50 -0300

    Acréscimo nome Thiago

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!<br>Fernanda<br>Gabriel Baptista<br>Brenda Trindade<br>Pedro Mendes<br>Amanda';
+    return 'Hello World!<br>Fernanda<br>Gabriel Baptista<br>Brenda Trindade<br>Pedro Mendes<br>Amanda<br>Thiago';
   }
 }
