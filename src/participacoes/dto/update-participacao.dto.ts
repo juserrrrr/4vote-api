7bc06a16d54cff3d35cb8aed5741c088e78e3155
LuisMarioRC <luismariocarneiro8@gmail.com>
2024-06-06 18:13:22 -0300

    refactor: Refatoração de código
    Co-authored-by: Gabriel Henry <GHenryssg@users.noreply.github.com>

--- src/participacoes/dto/update-participacao.dto.ts
+++ src/participacoes/dto/update-participacao.dto.ts
@@ -1,16 +1,4 @@
 import { PartialType } from '@nestjs/mapped-types';
-import { IsInt, IsNotEmpty, IsOptional } from 'class-validator';
+import { CreateParticipacaoDto } from './create-participacao.dto';
 
-export class CreatepartipacaoDto {
-  @IsInt()
-  @IsNotEmpty()
-  @IsOptional()
-  usuario_id?: number;
-
-  @IsInt()
-  @IsNotEmpty()
-  @IsOptional()
-  pesquisa_id?: number;
-}
-
-export class UpdateParticipacaoDto extends PartialType(CreatepartipacaoDto) {}
+export class UpdateParticipacaoDto extends PartialType(CreateParticipacaoDto) {}
