1d16572304fd9bb31f198e6314637466cff79393
avcsilva <avcsilva@ecomp.uefs.br>
2024-06-15 02:00:39 -0300

    feat: cria AuthGuard

---
+++ src/auth/auth.guard.ts
@@ -0,0 +1,28 @@
+import { CanActivate, ExecutionContext, Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { Request } from 'express';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+  constructor(private readonly jwtService: JwtService) {}
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const request = context.switchToHttp().getRequest();
+    const token = this.extractTokenFromHeader(request);
+    if (!token) {
+      throw new UnauthorizedException('Token não encontrado');
+    }
+    try {
+      const payload = this.jwtService.verify(token, { issuer: '4voteSignature' });
+      request.payload = payload;
+    } catch {
+      throw new UnauthorizedException('Token inválido');
+    }
+    return true;
+  }
+
+  private extractTokenFromHeader(request: Request): string | undefined {
+    const [type, token] = request.headers.authorization?.split(' ') ?? [];
+    return type === 'Bearer' ? token : undefined;
+  }
+}
