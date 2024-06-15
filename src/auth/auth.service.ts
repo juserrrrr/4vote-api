9b7f8731aee6967ae19da9483ef995469fa1c99f
avcsilva <avcsilva@ecomp.uefs.br>
2024-06-14 23:57:57 -0300

    feat: adiciona auth cadastro

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,8 +2,9 @@ import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { UsuariosService } from 'src/usuarios/usuarios.service';
-import { compare } from 'bcrypt';
+import { compare, genSalt, hash } from 'bcrypt';
 import { AuthLoginDto } from './dto/auth-login.dto';
+import { AuthCadastroDto } from './dto/auth-cadastro.dto';
 
 @Injectable()
 export class AuthService {
@@ -13,23 +14,39 @@ export class AuthService {
     private readonly usuarioService: UsuariosService,
   ) {}
 
+  criarToken({ id, nome }: { id: Number; nome: string }) {
+    const accessToken = this.jwtService.sign(
+      {
+        id,
+        nome,
+      },
+      {
+        expiresIn: '1d',
+        subject: String(id),
+        issuer: '4voteSignature',
+      },
+    );
+
+    return { accessToken };
+  }
+
   async login(loginDto: AuthLoginDto) {
     const usuario = await this.prisma.usuario.findUnique({ where: { email: loginDto.email } });
     if (!usuario || !compare(loginDto.senha, usuario.senha)) {
       throw new UnauthorizedException('Credenciais inválidas');
     }
-    return {
-      accessToken: this.jwtService.sign(
-        {
-          sub: usuario.id,
-          nome: usuario.nome,
-        },
-        {
-          expiresIn: '1d',
-          subject: String(usuario.id),
-          issuer: '4voteSignature',
-        },
-      ),
-    };
+    const payload = { id: usuario.id, nome: usuario.nome };
+    return this.criarToken(payload);
+  }
+
+  async cadastro(cadastroDto: AuthCadastroDto) {
+    const salt = await genSalt();
+    cadastroDto.senha = await hash(cadastroDto.senha, salt);
+    const usuario = await this.usuarioService.create(cadastroDto);
+    if (!usuario) {
+      throw new UnauthorizedException('Erro ao cadastrar usuário');
+    }
+    const payload = { id: usuario.id, nome: usuario.nome };
+    return this.criarToken(payload);
   }
 }
