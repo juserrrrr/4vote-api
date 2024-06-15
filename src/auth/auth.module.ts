f5234b2f5228e85e415d0b2f5a8b3327916434d2
avcsilva <avcsilva@ecomp.uefs.br>
2024-06-15 00:51:09 -0300

    feat: adiciona registro para JwtModule

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { PrismaModule } from 'src/prisma/prisma.module';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
   controllers: [AuthController],
   providers: [AuthService],
-  imports: [PrismaModule],
+  imports: [PrismaModule, JwtModule.register({ secret: process.env.JWT_SECRET })],
 })
 export class AuthModule {}
