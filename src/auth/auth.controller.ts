943df9b038d8905c911a6614f0664294c0bafe54
avcsilva <avcsilva@ecomp.uefs.br>
2024-06-15 00:38:46 -0300

    feat: adiciona auth cadastro

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,7 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthLoginDto } from './dto/auth-login.dto';
+import { AuthCadastroDto } from './dto/auth-cadastro.dto';
 
 @Controller('auth')
 export class AuthController {
@@ -9,5 +10,10 @@ export class AuthController {
   @Post('login')
   async login(@Body() loginDto: AuthLoginDto) {
     return this.authService.login(loginDto);
+  }
+
+  @Post('cadastro')
+  async cadastro(@Body() cadastroDto: AuthCadastroDto) {
+    return this.authService.cadastro(cadastroDto);
   }
 }
