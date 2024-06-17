ce36d24b2ea57ac9cd4d765b825accdcac911559
Vitor Costa <avcsilva@ecomp.uefs.br>
2024-06-17 17:04:20 -0300

    AV/Task034 AuthGuard (#31)
    * feat: adiciona JWT
    
    * feat: adiciona bcrypt
    
    * feat: adiciona arquivos para auth endpoint
    
    * Create auth-cadastro.dto.ts
    
    * feat: adiciona geração de token
    
    * update: atualiza formato dto
    
    * feat: adiciona types/bcrypt
    
    * feat: adiciona auth cadastro
    
    * feat: adiciona auth cadastro
    
    * feat: adiciona registro para JwtModule
    
    * feat: cria AuthGuard
    
    * feat: adiciona proteção AuthGuard
    
    * update: altera login -> entrar
    
    * update: altera idioma de alguns termos
    
    * update: importa modulo usuarios
    
    * update: altera idioma de alguns termos
    
    * update: altera funções e termos

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,19 @@
+import { Body, Controller, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { AuthCadastroDto } from './dto/auth-cadastro.dto';
+import { AuthEntrarDto } from './dto/auth-entrar.dto';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post('entrar')
+  async entrar(@Body() entrarDto: AuthEntrarDto) {
+    return this.authService.entrar(entrarDto);
+  }
+
+  @Post('cadastro')
+  async cadastro(@Body() cadastroDto: AuthCadastroDto) {
+    return this.authService.cadastro(cadastroDto);
+  }
+}
---
+++ src/auth/auth.service.ts
@@ -0,0 +1,52 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { UsuariosService } from 'src/usuarios/usuarios.service';
+import { compare, genSalt, hash } from 'bcrypt';
+import { AuthCadastroDto } from './dto/auth-cadastro.dto';
+import { AuthEntrarDto } from './dto/auth-entrar.dto';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private readonly prisma: PrismaService,
+    private readonly jwtService: JwtService,
+    private readonly usuarioService: UsuariosService,
+  ) {}
+
+  criarToken({ id, nome }: { id: Number; nome: string }) {
+    const accessToken = this.jwtService.sign(
+      {
+        id,
+        nome,
+      },
+      {
+        expiresIn: '1d',
+        subject: String(id),
+        issuer: 'Assinatura4Vote',
+      },
+    );
+
+    return { accessToken };
+  }
+
+  async entrar(entrarDto: AuthEntrarDto) {
+    const usuario = await this.prisma.usuario.findUnique({ where: { email: entrarDto.email } });
+    if (!usuario || !compare(entrarDto.senha, usuario.senha)) {
+      throw new UnauthorizedException('Credenciais inválidas');
+    }
+    const payload = { id: usuario.id, nome: usuario.nome };
+    return this.criarToken(payload);
+  }
+
+  async cadastro(cadastroDto: AuthCadastroDto) {
+    const salt = await genSalt();
+    cadastroDto.senha = await hash(cadastroDto.senha, salt);
+    const usuario = await this.usuarioService.create(cadastroDto);
+    if (!usuario) {
+      throw new UnauthorizedException('Erro ao cadastrar usuário');
+    }
+    const payload = { id: usuario.id, nome: usuario.nome };
+    return this.criarToken(payload);
+  }
+}
