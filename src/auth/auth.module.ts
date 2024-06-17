64033a6ca32e615dbde73104165bbe886bd5eef3
avcsilva <avcsilva@ecomp.uefs.br>
2024-06-17 14:28:42 -0300

    update: importa modulo usuarios

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,10 +3,11 @@ import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { PrismaModule } from 'src/prisma/prisma.module';
 import { JwtModule } from '@nestjs/jwt';
+import { UsuariosModule } from 'src/usuarios/usuarios.module';
 
 @Module({
   controllers: [AuthController],
   providers: [AuthService],
-  imports: [PrismaModule, JwtModule.register({ secret: process.env.JWT_SECRET })],
+  imports: [PrismaModule, JwtModule.register({ secret: process.env.JWT_SECRET }), UsuariosModule],
 })
 export class AuthModule {}
