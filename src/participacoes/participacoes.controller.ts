1a1fdbc76831139d902033d65a566d0e3ef074f2
brendatrindade <brendatrindde@gmail.com>
2024-06-20 17:08:44 -0300

    up remove route to create 'participacoes'
    Remove rota create do endpoint participacoes

--- src/participacoes/participacoes.controller.ts
+++ src/participacoes/participacoes.controller.ts
@@ -1,5 +1,4 @@
-import { Body, Controller, Delete, Get, Param, ParseIntPipe, Post, Put, UseGuards } from '@nestjs/common';
-import { CreateParticipacaoDto } from './dto/create-participacao.dto';
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, Put, UseGuards } from '@nestjs/common';
 import { UpdateParticipacaoDto } from './dto/update-participacao.dto';
 import { ParticipacaoService } from './participacao.service';
 import { AuthGuard } from 'src/auth/auth.guard';
@@ -17,11 +16,6 @@ export class ParcipacoesController {
   @Get(':id')
   getById(@Param('id', new ParseIntPipe()) id: number) {
     return this.participacaoService.getById(id);
-  }
-
-  @Post()
-  create(@Body() createParticipacaoDto: CreateParticipacaoDto) {
-    return this.participacaoService.create(createParticipacaoDto);
   }
 
   @Put(':id')
