320691bc18fc516dd29b37ec50b5c3cddc37e6f4
José Gabriel <gabriel-pontes09@hotmail.com>
2024-06-20 16:32:41 -0300

    feat: Add OpcaoModule, ParticipacoesModule, and PerguntasModule to app.module.ts
    The code changes include adding the OpcaoModule, ParticipacoesModule, and PerguntasModule to the app.module.ts file. This change enables the functionality related to options, participation, and questions in the application. It enhances the overall functionality and user experience of the system.
    
    Recent user commits and repository commits indicate a consistent use of the "feat" prefix for feature additions and "refactor" prefix for code refactoring.

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,9 +8,23 @@ import { TagModule } from './tag/tag.module';
 import { OpcaoVotadaModule } from './opcaovotada/opcaovotada.module';
 import { PesquisaModule } from './pesquisas/pesquisa.module';
 import { AuthModule } from './auth/auth.module';
+import { OpcaoModule } from './opcao/opcao.module';
+import { ParticipacoesModule } from './participacoes/participacoes.module';
+import { PerguntasModule } from './perguntas/perguntas.module';
 
 @Module({
-  imports: [UsuariosModule, VotoModule, TagPesquisaModule, TagModule, OpcaoVotadaModule, PesquisaModule, AuthModule],
+  imports: [
+    AuthModule,
+    OpcaoModule,
+    OpcaoVotadaModule,
+    ParticipacoesModule,
+    PerguntasModule,
+    PesquisaModule,
+    TagModule,
+    TagPesquisaModule,
+    UsuariosModule,
+    VotoModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
