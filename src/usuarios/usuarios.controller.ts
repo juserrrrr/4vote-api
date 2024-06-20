5c09bb1a738ceec3bba9dd75a390fbc05ef3f8a3
brendatrindade <brendatrindde@gmail.com>
2024-06-20 16:28:21 -0300

    up remove route to create users
    Remove rota create do endpoint usuários

--- src/usuarios/usuarios.controller.ts
+++ src/usuarios/usuarios.controller.ts
@@ -1,6 +1,5 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards } from '@nestjs/common';
+import { Controller, Get, Body, Patch, Param, Delete, UseGuards } from '@nestjs/common';
 import { UsuariosService } from './usuarios.service';
-import { CreateUsuarioDto } from './dto/create-usuario.dto';
 import { UpdateUsuarioDto } from './dto/update-usuario.dto';
 import { AuthGuard } from 'src/auth/auth.guard';
 
@@ -8,11 +7,6 @@ import { AuthGuard } from 'src/auth/auth.guard';
 @Controller('usuarios')
 export class UsuariosController {
   constructor(private readonly usuariosService: UsuariosService) {}
-
-  @Post()
-  create(@Body() createUsuarioDto: CreateUsuarioDto) {
-    return this.usuariosService.create(createUsuarioDto);
-  }
 
   @Get()
   findAll() {
