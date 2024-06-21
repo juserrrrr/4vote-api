33719de75524e1b211f1ab1c46e1aa9d353c0b91
LuisMarioRC <luismariocarneiro8@gmail.com>
2024-06-21 11:09:36 -0300

    feat: Criando consultas SQL para a pesquisa

--- src/pesquisas/pesquisa.service.ts
+++ src/pesquisas/pesquisa.service.ts
@@ -1,32 +1,66 @@
 import { Injectable } from '@nestjs/common';
 import { CreatePesquisaDto } from './dto/create-pesquisa.dto';
 import { UpdatePesquisaDto } from './dto/update-pesquisa.dto';
+import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
 export class PesquisaService {
-  create(body: CreatePesquisaDto) {
-    const retorno = Object.entries(body)
-      .map(([chave, valor]) => `${chave}: ${valor}`)
-      .join(', ');
-    return `Pesquisa criada: ${retorno}`;
+  constructor(private readonly prisma: PrismaService) {}
+
+  async create(body: CreatePesquisaDto) {
+    return await this.prisma.$executeRaw`
+    INSERT INTO Pesquisa (
+      titulo,
+      codigo,
+      dataCriacao,
+      dataTermino,
+      ehPublico,
+      descricao,
+      criador,
+      arquivado,
+      URLimagem,
+      ehVotacao
+      ) VALUES ( 
+      ${body.titulo}, 
+      ${body.codigo}, 
+      ${body.dataCriacao}, 
+      ${body.dataTermino}, 
+      ${body.ehPublico}, 
+      ${body.descricao}, 
+      ${body.criador}, 
+      ${body.arquivado}, 
+      ${body.URLimagem}, 
+      ${body.ehVotacao})`;
   }
 
-  findAll() {
-    return 'Retornando todas as pesquisas';
+  async findAll() {
+    return await this.prisma.$queryRaw`
+    SELECT * FROM Pesquisa`;
   }
 
-  getById(id: number) {
-    return `Pesquisa de id ${id} encontrada`;
+  async getById(id: number) {
+    return await this.prisma.$queryRaw`
+    SELECT * FROM Pesquisa WHERE id = ${id}`;
   }
 
-  update(body: UpdatePesquisaDto, id: number) {
-    const retorno = Object.entries(body)
-      .map(([chave, valor]) => `${chave}: ${valor}`)
-      .join(', ');
-    return `Dados atualizados para pesquisa de ID ${id}:\n ${retorno}`;
+  async update(body: UpdatePesquisaDto, id: number) {
+    return await this.prisma.$queryRaw`
+    UPDATE Pesquisa SET 
+    titulo = ${body.titulo}, 
+    codigo = ${body.codigo}, 
+    dataCriacao = ${body.dataCriacao}, 
+    dataTermino = ${body.dataTermino}, 
+    ehPublico =  ${body.ehPublico}, 
+    descricao = ${body.descricao}, 
+    criador = ${body.criador}, 
+    arquivado = ${body.arquivado}, 
+    URLimagem = ${body.URLimagem}, 
+    ehVotacao = ${body.ehVotacao} 
+    WHERE id = ${id}`;
   }
 
-  delete(id: number) {
-    return `Deletando pesquisa de ID ${id}`;
+  async delete(id: number) {
+    return await this.prisma.$executeRaw`
+    DELETE FROM Pesquisa WHERE id = ${id}`;
   }
 }
