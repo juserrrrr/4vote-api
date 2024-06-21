a0ac42043ff1fde4960e84bcd8c90cccf854ec8e
José Gabriel <89746639+juserrrrr@users.noreply.github.com>
2024-06-21 19:12:25 -0300

    refactor: Update import path for OpcaoVotadaModule in app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,7 @@ import { VotoModule } from './voto/voto.module';
 import { TagPesquisaModule } from './tagpesquisa/tagpesquisa.module';
 import { TagModule } from './tag/tag.module';
 import { PesquisaModule } from './pesquisas/pesquisa.module';
+import { AuthModule } from './auth/auth.module';
 import { OpcaoModule } from './opcao/opcao.module';
 import { ParticipacoesModule } from './participacoes/participacoes.module';
 import { PerguntasModule } from './perguntas/perguntas.module';
@@ -13,7 +14,8 @@ import { OpcaoVotadaModule } from './opcaoVotada/opcaovotada.module';
 
 @Module({
   imports: [
-    OpcaoModule,
+    // AuthModule,
+    // OpcaoModule,
     OpcaoVotadaModule,
     ParticipacoesModule,
     PerguntasModule,
