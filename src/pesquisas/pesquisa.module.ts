73231a8f1beedcba3e59449112ad0d3c5d3f99ed
LuisMarioRC <luismariocarneiro8@gmail.com>
2024-06-21 11:10:56 -0300

    perf: Importando o prismaService

--- src/pesquisas/pesquisa.module.ts
+++ src/pesquisas/pesquisa.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { PesquisaController } from './pesquisa.controller';
 import { PesquisaService } from './pesquisa.service';
+import { PrismaService } from '../prisma/prisma.service';
 
 @Module({
-  imports: [],
+  imports: [PrismaService],
   controllers: [PesquisaController],
   providers: [PesquisaService],
   exports: [],
