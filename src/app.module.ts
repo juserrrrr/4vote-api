fa926486f76da9a812c6aa9125534f430c045074
José Gabriel <89746639+juserrrrr@users.noreply.github.com>
2024-06-21 19:10:59 -0300

    refactor: Update import paths in app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,28 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsuariosModule } from './usuarios/usuarios.module';
 import { VotoModule } from './voto/voto.module';
+import { TagPesquisaModule } from './tagpesquisa/tagpesquisa.module';
+import { TagModule } from './tag/tag.module';
+import { PesquisaModule } from './pesquisas/pesquisa.module';
+import { OpcaoModule } from './opcao/opcao.module';
+import { ParticipacoesModule } from './participacoes/participacoes.module';
+import { PerguntasModule } from './perguntas/perguntas.module';
+import { OpcaoVotadaModule } from './opcaoVotada/opcaovotada.module';
 
 @Module({
-  imports: [VotoModule],
+  imports: [
+    OpcaoModule,
+    OpcaoVotadaModule,
+    ParticipacoesModule,
+    PerguntasModule,
+    PesquisaModule,
+    TagModule,
+    TagPesquisaModule,
+    UsuariosModule,
+    VotoModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
