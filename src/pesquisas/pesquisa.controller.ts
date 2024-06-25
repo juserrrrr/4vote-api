3b8df3fe94dc77a11703e2e48ce662d060f58d80
LuisMarioRC <luismariocarneiro8@gmail.com>
2024-06-25 14:05:17 -0300

    fix: Passando os novos dto's para o método create

--- src/pesquisas/pesquisa.controller.ts
+++ src/pesquisas/pesquisa.controller.ts
@@ -2,14 +2,21 @@ import { Controller, Get, Post, Delete, Patch, Body, Param, ParseIntPipe } from 
 import { PesquisaService } from './pesquisa.service';
 import { CreatePesquisaDto } from './dto/create-pesquisa.dto';
 import { UpdatePesquisaDto } from './dto/update-pesquisa.dto';
+import { CreatePerguntaDto } from '../perguntas/dto/create-pergunta.dto';
+import { CreateOpcaoDto } from '../opcao/dto/create-opcao.dto';
 
 @Controller('pesquisas')
 export class PesquisaController {
   constructor(private readonly pesquisaService: PesquisaService) {}
 
   @Post()
-  create(@Body() body: CreatePesquisaDto) {
-    return this.pesquisaService.create(body);
+  create(
+    @Body()
+    createPesquisaDto: CreatePesquisaDto,
+    createPerguntaDto: CreatePerguntaDto,
+    createOpcaoDto: CreateOpcaoDto,
+  ) {
+    return this.pesquisaService.create(createPesquisaDto, createPerguntaDto, createOpcaoDto);
   }
 
   @Get()
