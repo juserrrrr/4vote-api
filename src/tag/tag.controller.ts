e095f09c5b4f6b6806d81d9ff65284e91724239d
brendatrindade <brendatrindde@gmail.com>
2024-06-25 00:54:48 -0300

    up endpoint tags
    atualiza endpoint tags

--- src/tag/tag.controller.ts
+++ src/tag/tag.controller.ts
@@ -1,7 +1,5 @@
-import { Controller, Get, Body, Param, Put, Delete } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { TagService } from './tag.service';
-import { UpdateTagDto } from './dto/update-tag.dto';
-import { ValidationPipe } from '@nestjs/common';
 
 @Controller('tag')
 export class TagController {
@@ -15,15 +13,5 @@ export class TagController {
   @Get(':id')
   findOne(@Param('id') id: number) {
     return this.tagService.findOne(+id);
-  }
-
-  @Put(':id')
-  update(@Param('id') id: string, @Body(new ValidationPipe()) updateTagDto: UpdateTagDto) {
-    return this.tagService.update(+id, updateTagDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.tagService.remove(+id);
   }
 }
--- src/tag/tag.service.ts
+++ src/tag/tag.service.ts
@@ -1,5 +1,4 @@
 import { Injectable } from '@nestjs/common';
-import { UpdateTagDto } from './dto/update-tag.dto';
 
 @Injectable()
 export class TagService {
@@ -9,13 +8,5 @@ export class TagService {
 
   findOne(id: number): string {
     return `Aqui retorna a Tag com o id ${id.toString}`;
-  }
-
-  update(id: number, updateTagDto: UpdateTagDto): string {
-    return `Aqui retorna a tag alterada com o id ${id.toString()} para as novas informações do objeto com o id ${updateTagDto.id.toString}`;
-  }
-
-  remove(id: number): string {
-    return `Tag do id ${id.toString} deletada com sucesso`;
   }
 }
