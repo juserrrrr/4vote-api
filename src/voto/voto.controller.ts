fb4b4cbb64c62a0e6119ffd51a184c3fe573e84e
brendatrindade <brendatrindde@gmail.com>
2024-06-25 01:36:04 -0300

    up endpoint voto

--- src/voto/voto.controller.ts
+++ src/voto/voto.controller.ts
@@ -1,29 +1,12 @@
-import { Controller, Get, Body, Param, Put, Delete } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { VotoService } from './voto.service';
-import { UpdateVotoDto } from './dto/update-voto.dto';
-import { ValidationPipe } from '@nestjs/common';
 
 @Controller('voto')
 export class VotoController {
   constructor(private readonly votoService: VotoService) {}
 
-  @Get()
-  findAll() {
-    return this.votoService.findAll();
-  }
-
   @Get(':id')
   findOne(@Param('id') id: number) {
     return this.votoService.findOne(+id);
-  }
-
-  @Put(':id')
-  update(@Param('id') id: string, @Body(new ValidationPipe()) updateVotoDto: UpdateVotoDto) {
-    return this.votoService.update(+id, updateVotoDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.votoService.remove(+id);
   }
 }
--- src/voto/voto.service.ts
+++ src/voto/voto.service.ts
@@ -1,20 +1,8 @@
 import { Injectable } from '@nestjs/common';
-import { UpdateVotoDto } from './dto/update-voto.dto';
+
 @Injectable()
 export class VotoService {
-  findAll(): string {
-    return `Aqui retorna todos os votos`;
-  }
-
   findOne(id: number): string {
     return `Aqui retorna o voto com o id ${id.toString}`;
-  }
-
-  update(id: number, updateVotoDto: UpdateVotoDto): string {
-    return `Aqui retorna o voto alterado com o id ${id.toString()} para as novas informações do objeto com o id ${updateVotoDto.id.toString}`;
-  }
-
-  remove(id: number): string {
-    return `Voto do id ${id.toString} deletado com sucesso`;
   }
 }
