8640f3c12ba99d53df0a8fe00df63bfe558247ad
José Gabriel <89746639+juserrrrr@users.noreply.github.com>
2024-06-25 15:30:38 -0300

    chore: Update pesquisa module imports
    The code changes update the imports in the pesquisa.module.ts file to use the PrismaModule instead of the deprecated PrismaService. This improves the maintainability and organization of the code.

--- src/pesquisas/pesquisa.module.ts
+++ src/pesquisas/pesquisa.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
 import { PesquisaController } from './pesquisa.controller';
 import { PesquisaService } from './pesquisa.service';
-import { PrismaService } from '../prisma/prisma.service';
+import { PrismaModule } from '../prisma/prisma.module';
 
 @Module({
-  imports: [PrismaService],
   imports: [PrismaModule],
   controllers: [PesquisaController],
   providers: [PesquisaService],
