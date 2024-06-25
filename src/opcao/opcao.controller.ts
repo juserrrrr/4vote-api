df989114242a78adee4b19731adfca11d3c32244
brendatrindade <brendatrindde@gmail.com>
2024-06-25 00:52:56 -0300

    up endpoint opcao
    atualiza endpoint opcao

--- src/opcao/opcao.controller.ts
+++ src/opcao/opcao.controller.ts
@@ -1,7 +1,5 @@
-import { Controller, Get, Body, Param, Put, Delete, UseGuards } from '@nestjs/common';
+import { Controller, Get, Param, UseGuards } from '@nestjs/common';
 import { OpcaoService } from './opcao.service';
-import { UpdateOpcaoDto } from './dto/update-opcao.dto';
-import { ValidationPipe } from '@nestjs/common';
 import { AuthGuard } from 'src/auth/auth.guard';
 
 @UseGuards(AuthGuard)
@@ -9,23 +7,8 @@ import { AuthGuard } from 'src/auth/auth.guard';
 export class OpcaoController {
   constructor(private readonly opcaoService: OpcaoService) {}
 
-  @Get()
-  findAll() {
-    return this.opcaoService.findAll();
-  }
-
   @Get(':id')
   findOne(@Param('id') id: number) {
     return this.opcaoService.findOne(+id);
-  }
-
-  @Put(':id')
-  update(@Param('id') id: string, @Body(new ValidationPipe()) updateOpcaoDto: UpdateOpcaoDto) {
-    return this.opcaoService.update(+id, updateOpcaoDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.opcaoService.remove(+id);
   }
 }
--- src/opcao/opcao.service.ts
+++ src/opcao/opcao.service.ts
@@ -1,27 +1,8 @@
 import { Injectable } from '@nestjs/common';
-import { UpdateOpcaoDto } from './dto/update-opcao.dto';
-
-export interface Opcao {
-  id: number;
-  texto: string;
-  quantVotos: number;
-}
 
 @Injectable()
 export class OpcaoService {
-  findAll(): string {
-    return `Aqui retorna todas as opções`;
-  }
-
   findOne(id: number): string {
     return `Aqui retorna a opção com o id ${id.toString}`;
-  }
-
-  update(id: number, updateOpcaoDto: UpdateOpcaoDto): string {
-    return `Aqui retorna a opção alterada com sucesso do id ${id.toString()} para as novas informações do objeto com o id ${updateOpcaoDto.id.toString}`;
-  }
-
-  remove(id: number): string {
-    return `Opcao do id ${id.toString} deletada com sucesso`;
   }
 }
