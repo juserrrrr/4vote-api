d330dbdc3fd23d95e9c995f1c9576ad1f8863e8b
LuisMarioRC <luismariocarneiro8@gmail.com>
2024-06-24 23:16:33 -0300

    fix: Fazendo alterações para criar outras tabelas

--- src/pesquisas/pesquisa.service.ts
+++ src/pesquisas/pesquisa.service.ts
@@ -2,65 +2,63 @@ import { Injectable } from '@nestjs/common';
 import { CreatePesquisaDto } from './dto/create-pesquisa.dto';
 import { UpdatePesquisaDto } from './dto/update-pesquisa.dto';
 import { PrismaService } from '../prisma/prisma.service';
+import { CreatePerguntaDto } from 'src/perguntas/dto/create-pergunta.dto';
+import { CreateOpcaoDto } from 'src/opcao/dto/create-opcao.dto';
 
 @Injectable()
 export class PesquisaService {
   constructor(private readonly prisma: PrismaService) {}
 
-  async create(body: CreatePesquisaDto) {
-    return await this.prisma.$executeRaw`
-    INSERT INTO Pesquisa (
-      titulo,
-      codigo,
-      dataCriacao,
-      dataTermino,
-      ehPublico,
-      descricao,
-      criador,
-      arquivado,
-      URLimagem,
-      ehVotacao
-      ) VALUES ( 
-      ${body.titulo}, 
-      ${body.codigo}, 
-      ${body.dataCriacao}, 
-      ${body.dataTermino}, 
-      ${body.ehPublico}, 
-      ${body.descricao}, 
-      ${body.criador}, 
-      ${body.arquivado}, 
-      ${body.URLimagem}, 
-      ${body.ehVotacao})`;
+  async create(
+    createPesquisaDto: CreatePesquisaDto,
+    createPerguntaDto: CreatePerguntaDto,
+    createOpcaoDto: CreateOpcaoDto,
+  ) {
+    return await this.prisma.$transaction(async (prisma) => {
+      const pesquisa = await prisma.pesquisa.create({
+        data: createPesquisaDto,
+      });
+
+      const pergunta = await prisma.pergunta.create({
+        data: {
+          texto: createPerguntaDto.texto,
+          URLimagem: createPerguntaDto.URLimagem,
+          pesquisa_id: pesquisa.id,
+        },
+      });
+
+      const opcao = await prisma.opcao.create({
+        data: {
+          pergunta_id: pergunta.id,
+          texto: createOpcaoDto.texto,
+          quantVotos: createOpcaoDto.quantVotos,
+        },
+      });
+
+      return { pesquisa, pergunta, opcao };
+    });
   }
 
   async findAll() {
-    return await this.prisma.$queryRaw`
-    SELECT * FROM Pesquisa`;
+    return await this.prisma.pesquisa.findMany();
   }
 
   async getById(id: number) {
-    return await this.prisma.$queryRaw`
-    SELECT * FROM Pesquisa WHERE id = ${id}`;
+    return await this.prisma.pesquisa.findUnique({
+      where: { id },
+    });
   }
 
   async update(body: UpdatePesquisaDto, id: number) {
-    return await this.prisma.$queryRaw`
-    UPDATE Pesquisa SET 
-    titulo = ${body.titulo}, 
-    codigo = ${body.codigo}, 
-    dataCriacao = ${body.dataCriacao}, 
-    dataTermino = ${body.dataTermino}, 
-    ehPublico =  ${body.ehPublico}, 
-    descricao = ${body.descricao}, 
-    criador = ${body.criador}, 
-    arquivado = ${body.arquivado}, 
-    URLimagem = ${body.URLimagem}, 
-    ehVotacao = ${body.ehVotacao} 
-    WHERE id = ${id}`;
+    return await this.prisma.pesquisa.update({
+      where: { id },
+      data: body,
+    });
   }
 
   async delete(id: number) {
-    return await this.prisma.$executeRaw`
-    DELETE FROM Pesquisa WHERE id = ${id}`;
+    return await this.prisma.pesquisa.delete({
+      where: { id },
+    });
   }
 }
