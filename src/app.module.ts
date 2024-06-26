024243c73627ee4c70ebc9d0d5ff4ab90ef004f3
Gabriel Baptista <gabriel.costa.baptista1@gmail.com>
2024-06-26 01:01:27 -0300

    Update: adiciona mailer module e config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsuariosModule } from './usuarios/usuarios.module';
 import { PerguntasModule } from './perguntas/perguntas.module';
+import { MailerModule } from './mailer/mailer.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [UsuariosModule, PerguntasModule],
+  imports: [ConfigModule.forRoot(), UsuariosModule, PerguntasModule, MailerModule],
   controllers: [AppController],
   providers: [AppService],
 })
