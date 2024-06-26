fa0bc4d2bf7cd8bc01ae44f425a317d721ef85a0
José Gabriel <gabriel-pontes09@hotmail.com>
2024-06-26 15:30:37 -0300

    add config origin CORS

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['https://4vote-api.vercel.app/'],
+    origin: ['https://4vote-web.vercel.app/'],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
   });
   await app.listen(4000);
