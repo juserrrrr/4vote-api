d3b68889fb73371917337d8d55afef47dfa2946c
brendatrindade <brendatrindde@gmail.com>
2024-07-08 14:50:12 -0300

    create fileDTO
    criado FileDTO para recebimento de arquivos

--- src/modules/upload/upload.controller.ts
+++ src/modules/upload/upload.controller.ts
@@ -1,7 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
 import { UploadService } from './upload.service';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { FileDTO } from './upload.dto';
 
 @Controller('upload')
 export class UploadController {
   constructor(private readonly uploadService: UploadService) {}
+
+  @Post('/')
+  @UseInterceptors(FileInterceptor('file'))
+  async uploadFile(@UploadedFile() file: FileDTO) {
+    console.log(file);
+  }
 }
---
+++ src/modules/upload/upload.dto.ts
@@ -0,0 +1,7 @@
+export type FileDTO = {
+  fieldname: string;
+  originalname: string;
+  mimetype: string;
+  buffer: Buffer;
+  size: number;
+};
