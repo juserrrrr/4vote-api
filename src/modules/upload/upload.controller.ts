2e06e3092814377afe0ea332ab6d07756e54c780
brendatrindade <brendatrindde@gmail.com>
2024-07-08 14:38:15 -0300

    add module upload
    cria modulo upload

---
+++ src/modules/upload/upload.controller.ts
@@ -0,0 +1,7 @@
+import { Controller } from '@nestjs/common';
+import { UploadService } from './upload.service';
+
+@Controller('upload')
+export class UploadController {
+  constructor(private readonly uploadService: UploadService) {}
+}
