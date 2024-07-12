f7c8b0e11b576bdcb2d20d724a13945d04395fc0
Gabriel Baptista <gabriel.costa.baptista1@gmail.com>
2024-07-12 01:35:02 -0300

    update: move @UseGuards
    Co-Authored-By: Gabriel Henry <142272107+GHenryssg@users.noreply.github.com>

--- src/usuarios/usuarios.controller.ts
+++ src/usuarios/usuarios.controller.ts
@@ -3,16 +3,17 @@ import { UsuariosService } from './usuarios.service';
 import { UpdateUsuarioDto } from './dto/update-usuario.dto';
 import { AuthGuard } from '../auth/auth.guard';
 
-@UseGuards(AuthGuard)
 @Controller('usuarios')
 export class UsuariosController {
   constructor(private readonly usuariosService: UsuariosService) {}
 
+  @UseGuards(AuthGuard)
   @Get('me')
   findMe(@Headers() headers) {
     return this.usuariosService.findMe(headers);
   }
 
+  @UseGuards(AuthGuard)
   @Patch('me')
   update(@Headers() headers, @Body() updateUsuarioDto: UpdateUsuarioDto) {
     return this.usuariosService.update(headers, updateUsuarioDto);
