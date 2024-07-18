4df1a8efdc09a1210eeaa4e8f7dd3ce4677d853e
LuisMarioRC <luismariocarneiro8@gmail.com>
2024-07-18 19:00:47 -0300

    fix: corrigindo caminho do create-voto-dto

--- src/participacoes/dto/create-participacao.dto.ts
+++ src/participacoes/dto/create-participacao.dto.ts
@@ -1,6 +1,6 @@
 import { Type } from 'class-transformer';
 import { IsNotEmpty, ValidateNested } from 'class-validator';
-import { CreateVotoDto } from 'src/voto/dto/create-voto.dto';
+import { CreateVotoDto } from '../../voto/dto/create-voto.dto';
 
 export class CreateParticipacaoDto {
   @IsNotEmpty()
