e9e7ebe7558a2cb2f8f38caf860b212897eb3272
Gabriel Baptista <gabriel.costa.baptista1@gmail.com>
2024-07-18 02:30:24 -0300

    update: adiciona função de recuperar senha
    Co-Authored-By: Gabriel Henry <142272107+GHenryssg@users.noreply.github.com>

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,10 +1,11 @@
-import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { Injectable, UnauthorizedException, NotFoundException } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { compare, genSalt, hash } from 'bcrypt';
 import { AuthCadastroDto } from './dto/auth-cadastro.dto';
 import { AuthEntrarDto } from './dto/auth-entrar.dto';
 import { PrismaService } from '../prisma/prisma.service';
 import { UsuariosService } from '../usuarios/usuarios.service';
+import { MailerService } from '../mailer/mailer.service';
 
 @Injectable()
 export class AuthService {
@@ -12,6 +13,7 @@ export class AuthService {
     private readonly prisma: PrismaService,
     private readonly jwtService: JwtService,
     private readonly usuarioService: UsuariosService,
+    private readonly mailerService: MailerService,
   ) {}
 
   criarToken({ id, nome }: { id: number; nome: string }) {
@@ -50,5 +52,48 @@ export class AuthService {
     }
     const payload = { id: usuario.id, nome: usuario.nome };
     return this.criarToken(payload);
+  }
+
+  // Função de recuperação de senha
+  async recuperarSenha(email: string) {
+    // Procura pelo usuário no banco de dados com base no email fornecido
+    const usuario = await this.prisma.usuario.findUnique({
+      where: { email },
+    });
+
+    // Se o usuário não for encontrado, lança uma exceção NotFoundException
+    if (!usuario) {
+      throw new NotFoundException('Usuário não encontrado');
+    }
+
+    // Gera uma nova senha aleatória de 8 caracteres
+    const novaSenha = Math.random().toString(36).slice(-8);
+
+    // Gera um salt e faz o hash da nova senha
+    const salt = await genSalt();
+    const hashedSenha = await hash(novaSenha, salt);
+
+    // Atualiza a senha do usuário no banco de dados com a nova senha hasheada
+    await this.prisma.usuario.update({
+      where: { email },
+      data: { senha: hashedSenha },
+    });
+
+    // Carrega o template de email de recuperação de senha
+    const template = this.mailerService.loadTemplate('password-reset');
+    const replacements = {
+      nome: usuario.nome, // Nome do usuário para personalizar o email
+      novaSenha, // Nova senha para informar ao usuário
+    };
+
+    // Envia o email de recuperação de senha usando o serviço de email
+    await this.mailerService.sendEmail({
+      recipients: [{ name: usuario.nome, address: email }],
+      subject: 'Recuperação de Senha', // Assunto do email
+      html: this.mailerService.template(template, replacements), // Conteúdo do email com placeholders substituídos
+    });
+
+    // Retorna uma mensagem de sucesso
+    return { message: 'Nova senha enviada para o email' };
   }
 }
