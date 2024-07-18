b9613352f1653b46cc01e6767220489e5b8346d4
Ilson <ilsonetoneto06@gmail.com>
2024-07-18 09:52:52 -0300

    refactor: changing methods to accept bucket name

--- src/modules/upload/upload.service.ts
+++ src/modules/upload/upload.service.ts
@@ -4,20 +4,21 @@ import { createClient } from '@supabase/supabase-js';
 
 @Injectable()
 export class UploadService {
-  async upload(file: FileDTO) {
-    const supabaseURL = 'https://nydqbchcfgkevfdmbifx.supabase.co';
-    const supabaseKEY =
-      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im55ZHFiY2hjZmdrZXZmZG1iaWZ4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjA0NjE0MDksImV4cCI6MjAzNjAzNzQwOX0.BJzPGLiJcIotjfUjXRONQMjQFKLMKvCwc1MaYMTJBIM';
+  private supabaseURL = 'https://nydqbchcfgkevfdmbifx.supabase.co';
+  private supabaseKEY =
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im55ZHFiY2hjZmdrZXZmZG1iaWZ4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjA0NjE0MDksImV4cCI6MjAzNjAzNzQwOX0.BJzPGLiJcIotjfUjXRONQMjQFKLMKvCwc1MaYMTJBIM';
+  private supabase = createClient(this.supabaseURL, this.supabaseKEY, {
+    auth: { persistSession: false },
+  });
 
-    const supabase = createClient(supabaseURL, supabaseKEY, {
-      auth: {
-        persistSession: false,
-      },
-    });
+  async upload(file: FileDTO, bucketName: string) {
+    const { data, error } = await this.supabase.storage
+      .from(bucketName)
+      .upload(file.originalname, file.buffer, { upsert: true });
 
-    const data = await supabase.storage.from('Files4vote').upload(file.originalname, file.buffer, {
-      upsert: true,
-    });
+    if (error) {
+      throw new Error(`Erro ao fazer upload do arquivo: ${error.message}`);
+    }
 
     return data;
   }
