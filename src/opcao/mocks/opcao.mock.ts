cfe22f715446cf562e131597ba6f554d6c39d430
brendatrindade <brendatrindde@gmail.com>
2024-07-22 04:12:08 -0300

    create opcaoMock
    cria mocks para simular diferentes opcoes

---
+++ src/opcao/mocks/opcao.mock.ts
@@ -0,0 +1,8 @@
+import { CreateOpcaoDto } from '../dto/create-opcao.dto';
+
+export const opcaoMock1: CreateOpcaoDto = {
+  texto: 'TextoOpcaoMock 1',
+};
+export const opcaoMock2: CreateOpcaoDto = {
+  texto: 'TextoOpcaoMock 2',
+};
